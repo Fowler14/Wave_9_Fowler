ce7fe7ce7ddda030e07a845ef5175e47ac8e7cb8
Fowler14 <rgfowler97@outlook.com>
2026-04-06 19:34:35 -0400

    Mopdule 1 HW

--- JavaScript_Foundations_Mastery/01_Variables/mentee/homework.js
+++ JavaScript_Foundations_Mastery/01_Variables/mentee/homework.js
@@ -4,16 +4,16 @@
 // Open DevTools → Console to see your results.
 
 // STEP 1: Create a variable called fullName (string) with your full name.
-
+let fullName = "Ryan Fowler";
 // STEP 2: Create a variable called profileAge (number) with your age.
-
+let profileAge = 28;
 // STEP 3: Create a variable called country (string) with the country you live in.
-
+let country = "USA";
 // STEP 4: Create a variable called favoriteHobby (string) with a hobby you enjoy.
-
+let favoriteHobby = "Golf";
 // STEP 5: Create a variable called isStudent (boolean) with true or false,
 //         depending on whether you consider yourself a student right now.
-
+let isStudent = true;
 // STEP 6: Use console.log to print the profile in a clear format, for example:
 //         --- PROFILE CARD ---
 //         Name: (your name)
@@ -21,7 +21,13 @@
 //         Country: (your country)
 //         Hobby: (your hobby)
 //         Is student? (true/false)
-
+console.log("--Profile Card--")
+console.log("Name: " + fullName +
+          "\nAge: " + profileAge +
+          "\nCountry: " + country +
+          "\nFavorite Hobby: " + favoriteHobby +
+          "\nIs student? " + isStudent)
+          
 // STEP 7: Change at least ONE of your variables (for example, favoriteHobby)
 //         to a new value and then log the entire profile card again
 //         to show the updated information.
