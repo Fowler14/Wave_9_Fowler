3faeb918af1505193f157c58acf52d25409b4873
Fowler14 <rgfowler97@outlook.com>
2026-05-12 16:43:59 -0400

    Did 09 Dom Mini Tasks HW

--- JavaScript_Foundations_Mastery/09_DOM_MiniTasks/mentee/homework.js
+++ JavaScript_Foundations_Mastery/09_DOM_MiniTasks/mentee/homework.js
@@ -4,31 +4,51 @@
 
 // STEP 1: Select the heading element with id "moviesTitle" and store
 //         it in a variable.
-
+const moviesTitleEl = document.querySelector("#moviesTitle");
 // STEP 2: Select the paragraph with id "moviesInfo" and store it
 //         in a variable.
-
+const moviesInfoEl = document.querySelector("#moviesInfo");
 // STEP 3: Select the list with id "moviesList" and store it in a variable.
+const moviesListEl = document.getElementById("moviesList");
 
 // STEP 4: Change the textContent of moviesTitle to a new heading,
 //         for example "Movie List".
 
+moviesTitleEl.textContent = "Movie List";
+
+
 // STEP 5: Use moviesList.children.length (or querySelectorAll)
 //         to count how many movie items there are.
 //         Store this number in a variable (for example, movieCount).
+const movieCount = moviesList.children.length;
+console.log(movieCount);
 
 // STEP 6: Update moviesInfo.textContent to show a message like:
 //         "You currently have X favorite movies in your list."
 //         (Use the value of movieCount in the message.)
-
+moviesInfoEl.textContent = `You currently have ${movieCount} favorite movies.`;
 // STEP 7: Create a new li element for another movie using
 //         document.createElement("li").
 //         - Set its textContent to a new movie name.
 //         - Add the class "movie-item" to it.
 //         - Append it to moviesList.
+const newLiEl = document.createElement("li");
+newLiEl.textContent = "Interstellar";
+newLiEl.classList.add("movie-item");
+moviesListEl.appendChild(newLiEl);
+// const movieCount = moviesList.children.length;
+// console.log(movieCount);
 
 // STEP 8: Select one of the existing movie items (for example,
 //         the first one) and remove it using .remove().
 
+const removeItem =  moviesListEl.querySelectorAll("li");
+if (removeItem[2]) {
+    removeItem[2].remove();
+}
+
+
 // STEP 9: (Optional) After adding/removing, update moviesInfo again
 //         to show the new total number of movies.
+// const movieCount = moviesList.children.length;
+// moviesInfoEl.textContent = `You currently have ${movieCount} favorite movies.`;
