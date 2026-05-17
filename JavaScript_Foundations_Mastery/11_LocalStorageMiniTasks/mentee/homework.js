81ed5a0274fa106d7941668e74ced1cb74049a75
Fowler14 <rgfowler97@outlook.com>
2026-05-17 12:40:11 -0400

    Did 11_LocalStorageMiniTasks homework

--- JavaScript_Foundations_Mastery/11_LocalStorageMiniTasks/mentee/homework.js
+++ JavaScript_Foundations_Mastery/11_LocalStorageMiniTasks/mentee/homework.js
@@ -8,7 +8,12 @@
 //         - saveQuoteBtn (id "saveQuoteBtn")
 //         - clearQuoteBtn (id "clearQuoteBtn")
 //         - quoteDisplay (id "quoteDisplay")
-//
+
+const quoteInput = document.getElementById("quoteInput");
+const saveQuoteBtn = document.getElementById("saveQuoteBtn");
+const clearQuoteBtn = document.getElementById("clearQuoteBtn");
+const quoteDisplay = document.getElementById("quoteDisplay");
+
 // STEP 2: When the script loads:
 //         - Read localStorage.getItem("favoriteQuote").
 //         - If a quote exists (not null):
@@ -16,7 +21,16 @@
 //             * Also set quoteInput.value to that quote.
 //           Else:
 //             * Keep the default "No quote saved yet." message.
-//
+const storedQuote = localStorage.getItem("favoriteQuote");
+
+if (storedQuote !== null) {
+    quoteDisplay.textContent = storedQuote;
+    quoteInput.value = storedQuote;
+} else {
+    quoteDisplay.textContent = "No quote saved yet.";
+}
+
+
 // STEP 3: Add a "click" event listener to saveQuoteBtn.
 //         Inside the listener:
 //         - Read the current value from quoteInput.
@@ -24,9 +38,23 @@
 //         - Update quoteDisplay.textContent so it shows the saved quote.
 //         - (Optional) If the input is empty, you can decide not to save
 //           or show a custom message.
-//
+saveQuoteBtn.addEventListener("click", () => {
+    const currentQuote = quoteInput.value;
+
+    if (currentQuote === "") {
+        quoteDisplay.textContent = "Waiting for quote";
+        return;
+    }
+    localStorage.setItem("favoriteQuote", currentQuote);
+    quoteDisplay.textContent = currentQuote;
+})
 // STEP 4: Add a "click" event listener to clearQuoteBtn.
 //         Inside the listener:
 //         - Use localStorage.removeItem("favoriteQuote").
 //         - Clear the textarea (set value to an empty string).
 //         - Reset quoteDisplay.textContent to "No quote saved yet."
+clearQuoteBtn.addEventListener("click", () => {
+    localStorage.removeItem("favoriteQuote");
+    quoteInput.value = "";
+    quoteDisplay.textContent = "No Quote Saved Yet."
+})
